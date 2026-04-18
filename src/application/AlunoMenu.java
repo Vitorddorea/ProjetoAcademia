79c1bd4f30244b820ec1f488d9899e81373c7f38
Marina Santos <marinasmorais07@gmail.com>
2026-04-18 17:51:05 -0300

    Adicionando métodos na classe aluno e aluno.menu

--- src/application/AlunoMenu.java
+++ src/application/AlunoMenu.java
@@ -1,5 +1,6 @@
 package application;
 
+
 import entities.Aluno;
 import util.AlunoService;
 
@@ -12,7 +13,13 @@ public class AlunoMenu {
 
         Locale.setDefault(Locale.US);
         ArrayList<Aluno> listaAlunos = new ArrayList<>();
-
+        
+        Aluno aluno1 = new Aluno("Marina","345543334554", "27032007","11911324567","maria@gmail.com", "sim" );
+        Aluno aluno2 = new Aluno("Maria","34455334554", "27042007","1191455665467","mariass@gmail.com", "sim" );
+        
+        listaAlunos.add(aluno1);
+        listaAlunos.add(aluno2);
+        
         while (true){
             System.out.println("\n==== GERENCIAR ALUNO ====");
             System.out.println("1- Cadastrar aluno");
@@ -30,12 +37,18 @@ public class AlunoMenu {
                     listaAlunos.add(aluno);
                     break;
                 case 2:
-                    System.out.println("Lista de alunos ");
+                	Aluno.listarAlunos(listaAlunos);
+                	break;
                 case 3:
-                    System.out.println("Atualizar aluno");
+                	System.out.println("Atualizar aluno:");
+                    Aluno.atualizarAluno(sc, listaAlunos);
                     break;
                 case 4:
-                    System.out.println("Excluir aluno");
+                	System.out.println("Excluir aluno:");
+                    System.out.print("Digite o CPF: ");
+                    String cpf = sc.nextLine();
+
+                    Aluno.excluirAluno(listaAlunos, cpf);
                     break;
                 case 0:
                     return;
--- src/entities/Aluno.java
+++ src/entities/Aluno.java
@@ -1,6 +1,9 @@
 package entities;
 
+import java.util.Scanner;
 import java.time.LocalDate;
+import java.util.ArrayList;
+
 
 public class Aluno extends Pessoa{
 
@@ -29,7 +32,7 @@ public class Aluno extends Pessoa{
         return cpf;
     }
 
-    public void setCpf(String cpf) {
+    public void setCpf() {
         this.cpf = cpf;
     }
 
@@ -81,5 +84,60 @@ public class Aluno extends Pessoa{
         System.out.println("E-mail: " + getEmail());
         System.out.println("Plano ativo: " + getPlanoAtivo());
     }
+    
+    public static void listarAlunos(ArrayList <Aluno> listaAlunos) {
+        
+        if (listaAlunos.isEmpty()) {
+            System.out.println("Nenhum aluno cadastrado.");
+            return;
+        }
+        System.out.println("LISTA DE ALUNOS");
 
+        for (Aluno aluno : listaAlunos) {
+            System.out.println("Nome: " + aluno.getNome());
+            System.out.println("CPF: " + aluno.getCpf());
+            System.out.println("Telefone: " + aluno.getTelefone());
+            System.out.println("Email: " + aluno.getEmail());
+            System.out.println("Plano ativo: " + aluno.getPlanoAtivo());
+            System.out.println("-------------------------");
+        }
+    }
+    public static void excluirAluno(ArrayList<Aluno> listaAlunos, String cpf) {
+
+        for (int i = 0; i < listaAlunos.size(); i++) {
+            if (listaAlunos.get(i).getCpf().equals(cpf)) {
+                listaAlunos.remove(i);
+                System.out.println("Aluno removido com sucesso.");
+                return;
+            }
+        }
+
+        System.out.println("Aluno não encontrado.");
+    }
+    public static void atualizarAluno(Scanner sc, ArrayList<Aluno> listaAlunos) {
+
+        System.out.print("Digite o CPF do aluno que deseja atualizar: ");
+        String cpf = sc.nextLine();
+
+        for (Aluno aluno : listaAlunos) {
+            if (aluno.getCpf().equals(cpf)) {
+
+                System.out.print("Novo nome: ");
+                aluno.setNome(sc.nextLine());
+
+                System.out.print("Novo telefone: ");
+                aluno.setTelefone(sc.nextLine());
+
+                System.out.print("Novo email: ");
+                aluno.setEmail(sc.nextLine());
+
+                System.out.print("Novo plano: ");
+                aluno.setPlanoAtivo(sc.nextLine());
+
+                System.out.println("Aluno atualizado com sucesso.");
+                return;
+            }
+        }
+        System.out.println("Aluno não encontrado.");
+    }
 }
